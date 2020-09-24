e9c9d78c9ac60a81975b45090d45463e5ceeebb2
cse92 <tanveer.cse01@gmail.com>
2020-09-24 15:53:28 +0600

    tanu

---
+++ app/src/main/java/com/codian/femalecollection/UI/Product_description.java
@@ -0,0 +1,16 @@
+package com.codian.femalecollection.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codian.femalecollection.R;
+
+public class Product_description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_description);
+    }
+}
