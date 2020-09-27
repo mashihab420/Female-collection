31eb4960e4b601b09681808511c4469b3d5c4c07
Shihab <mahbubul15-8821@diu.edu.bd>
2020-09-27 13:29:13 +0600

    show product

--- app/src/main/java/com/codian/femalecollection/UI/activity/Product_description.java
+++ app/src/main/java/com/codian/femalecollection/UI/activity/Product_description.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.codian.femalecollection.R;
@@ -39,6 +42,21 @@ public class Product_description extends AppCompatActivity {
         binding.textView7.setText(price);
         binding.textView14.setText(des);
 
+
+    }
+
+    public void addToCart(View view) {
+
+      //  String size = (binding.radioGroup.getCheckedRadioButtonId()
+
+
+       if (binding.radioGroup.getCheckedRadioButtonId()==-1){
+           Toast.makeText(this, "You Must Select Dress Size ", Toast.LENGTH_SHORT).show();
+       }else{
+           String value = ((RadioButton)findViewById(binding.radioGroup.getCheckedRadioButtonId())).getText().toString();
+
+           Toast.makeText(this, ""+value, Toast.LENGTH_SHORT).show();
+       }
 
     }
 }
